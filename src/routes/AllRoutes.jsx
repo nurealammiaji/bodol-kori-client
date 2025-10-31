ce95021216e38d823f1057552add734215b3c067
nurealammiaji <nurealam.miaji@gmail.com>
2025-10-31 12:33:00 +0600

    new route added

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,11 +1,17 @@
-import App from './../App';
 import { createBrowserRouter } from "react-router-dom";
+import App from './../App';
+import Home from '../components/Home/Home';
 
 const AllRoutes = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-        children: []
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
     }
 ])
 
