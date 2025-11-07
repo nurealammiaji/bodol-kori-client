a7508cc6a1be33ae572a683faa951877f6138800
nurealammiaji <nurealam.miaji@gmail.com>
2025-11-07 21:53:52 +0600

    tailwind daisy ui added

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='text-2xl text-red-500'>
+        This test
+      </div>
+    </div>
+  )
+}
+
+export default Home
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from './../App';
-import Home from '../components/Home/Home';
+import Home from "../pages/Home/Home";
+
 
 const AllRoutes = createBrowserRouter([
     {
